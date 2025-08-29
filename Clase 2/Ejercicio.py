eb580419ae686cd0df611e8cce564598470a3a2c
IgnacioAScotto <scottoignacioagustin@gmail.com>
2025-08-28 23:17:11 -0300

    Clase 2 terminada

---
+++ Clase 2/Ejercicio.py
@@ -0,0 +1,12 @@
+nombre = input('Ingresa tu Nombre: ')
+apellido = input("Ingresa tu Apellido: ")
+edad = input("Ingresa tu edad: ")
+email = input("Ingresa tu correo electronico: ")
+
+#Pasamos a mostrar los datos almacenados en las variables:
+
+print("---Tarjeta de presentacion:---")
+print("Nombre completo:", nombre, apellido)
+print("Edad:", edad, "años")
+print('Correo electronico:', email)
+print('-------------------------------------')
