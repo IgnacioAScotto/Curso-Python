d687e27c9495b5bc1a3fbdf076593cadb8eab6b9
IgnacioAScotto <scottoignacioagustin@gmail.com>
2025-09-27 17:26:59 -0300

    se creo la carpeta para la clase 5

---
+++ Clase 5/Ejercicio.py
@@ -0,0 +1,17 @@
+'''Para este ejercicio necesitamos un software que ayude a registrar y calcular información
+Para este ejercicio necesitamos un software que ayude a registrar y calcular información financiera básica para nuestros y nuestras clientes.
+
+Tu tarea para esta semana es la siguiente:
+
+1. Registrar los ingresos mensuales de un cliente durante 6 meses usando un bucle while para solicitar el ingreso de cada mes. 
+    Validar que los ingresos sean números positivos. Si se ingresa un valor negativo,
+    mostrá un mensaje indicando que el valor no es válido y volvé a pedir el dato.
+
+2. Calcular el total acumulado durante los 6 meses y el promedio mensual.
+    Mostrá este resultado al final del programa. '''
+
+mes = 1
+total_ingresos = 0
+
+while mes <= 6:
+    
